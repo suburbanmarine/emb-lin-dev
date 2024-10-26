c5515da80958d30f1e6863db7b3d38e40955fbb9
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-10-26 14:23:42 -0700

    initial can bus code

--- include/emb-lin-dev/BIC_2200.hpp
+++ include/emb-lin-dev/BIC_2200.hpp
@@ -8,3 +8,50 @@
 */
 
 #pragma once
+
+class BIC_2200
+{
+public:
+
+	enum class CMD_ID: uint16_t
+	{
+		OPERATION            = 0x0000U, 
+		VOUT_SET             = 0x0020U,
+		IOUT_SET             = 0x0030U,
+		FAULT_STATUS         = 0x0040U,
+		READ_VIN             = 0x0050U,
+		READ_VOUT            = 0x0060U,
+		READ_IOUT            = 0x0061U,
+		READ_TEMPERATURE_1   = 0x0062U,
+		READ_FAN_SPEED_1     = 0x0070U,
+		READ_FAN_SPEED_2     = 0x0071U,
+		MFR_ID_B0B5          = 0x0080U,
+		MFR_ID_B6B11         = 0x0081U,
+		MFR_MODEL_B0B5       = 0x0082U,
+		MFR_MODEL_B6B11      = 0x0083U,
+		MFR_REVISION_B0B5    = 0x0084U,
+		MFR_LOCATION_B0B2    = 0x0085U,
+		MFR_DATE_B0B5        = 0x0086U,
+		MFR_SERIAL_B0B5      = 0x0087U,
+		MFR_SERIAL_B6B11     = 0x0088U,
+		SCALING_FACTOR       = 0x00C0U,
+		SYSTEM_STATUS        = 0x00C1U, 
+		SYSTEM_CONFIG        = 0x00C2U,
+		DIRECTION_CTRL       = 0x0100U,
+		REVERSE_VOUT_SET     = 0x0120U,
+		REVERSE_IOUT_SET     = 0x0130U,
+		BIDIRECTIONAL_CONFIG = 0x0140U
+	};
+
+	BIC_2200();
+	~BIC_2200();
+
+	bool open(const std::string& iface);
+	bool close();
+protected:
+
+	bool wait_tx_can_packet(const std::chrono::microseconds& max_wait_time, const can_frame& frame);
+	bool wait_rx_can_packet(const std::chrono::microseconds& max_wait_time, can_frame* const out_frame);
+
+	int m_fd;
+};
--- src/BIC_2200.cpp
+++ src/BIC_2200.cpp
@@ -8,3 +8,111 @@
 */
 
 #include "emb-lin-dev/BIC_2200.hpp"
+
+BIC_2200::BIC_2200()
+{
+	m_fd = -1;
+}
+BIC_2200::~BIC_2200()
+{
+	close();
+}
+
+bool open(const std::string& iface)
+{
+	if(m_fd >= 0)
+	{
+		close();
+	}
+
+	sockaddr_can addr;
+	memset(&addr, 0, sizeof(addr));
+	ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+
+	m_fd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if(m_fd < 0)
+	{
+		return false;
+	}
+
+	strcpy(ifr.ifr_name, iface.c_str() );
+	int ret = ioctl(m_fd, SIOCGIFINDEX, &ifr);
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	ret = bind(m_fd, (sockaddr*)&addr, sizeof(addr));
+	if(ret < 0)
+	{
+		return false;
+	}
+
+    can_err_mask_t err_mask = CAN_ERR_MASK;
+    ret = setsockopt(m_fd, SOL_CAN_RAW, CAN_RAW_ERR_FILTER, &err_mask, sizeof(err_mask));
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+bool close()
+{
+	if(m_fd < 0)
+	{
+		return true;
+	}
+
+	int ret = close(m_fd);
+	m_fd    = -1;
+
+	return ret == 0;
+}
+
+
+bool wait_tx_can_packet(const std::chrono::microseconds& max_wait_time, const can_frame& tx_frame)
+{
+	//TODO: use epoll or poll to handle timeout for ability to enqueue frame
+
+	ssize_t len = write(m_fd, tx_frame, sizeof(can_frame));
+
+	if (len < 0)
+	{
+		// todo log errno
+        return false;
+	}
+
+	if (len != sizeof(can_frame) )
+	{
+		// underrun?
+		return false;
+	}
+
+	return true;
+}
+
+bool wait_rx_can_packet(const std::chrono::microseconds& max_wait_time, can_frame* const out_rx_frame)
+{
+	//TODO: use epoll or poll to handle timeout for new frame
+
+	ssize_t len = read(m_fd, out_rx_frame, sizeof(can_frame));
+
+	if (len < 0)
+	{
+		// todo log errno
+        return false;
+	}
+
+	if (len != sizeof(can_frame) )
+	{
+		// underrun?
+		return false;
+	}
+
+	return true;
+}
