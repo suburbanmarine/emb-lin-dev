b7a17046de3903c1a566ad50c0d3166597228704
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-11-24 00:27:12 -0800

    density codes

--- include/emb-lin-dev/M24XXX_DRE_base.hpp
+++ include/emb-lin-dev/M24XXX_DRE_base.hpp
@@ -17,14 +17,62 @@
 #include <cstddef>
 #include <cstdint>
 
-template<size_t SIZE, size_t PAGE_SIZE, size_t ADDR_SIZE>
+struct M24XXX_DRE_Properties
+{
+	size_t density_code; // so far, 2^density_code == size, but it could change
+	size_t size;
+	size_t page_size;
+	size_t addr_size;
+};
+
 class M24XXX_DRE_base : public I2C_dev_base
 {
 public:
-	typedef std::array<uint8_t, PAGE_SIZE> Pagebuffer;
-	typedef std::array<uint8_t, 3> Device_id_code;
 
-	M24XXX_DRE_base(const std::shared_ptr<I2C_bus_base>& bus, const long id) : I2C_dev_base(bus, id)
+	// For Device ID code
+	static constexpr size_t MF_CODE     = 0x20U; // ST
+	static constexpr size_t FAMILY_CODE = 0xE0U; // M24 series
+
+	// ID -> size
+	static constexpr std::map<size_t, size_t> DEVICE_SIZE = {
+		{0x08,   256},
+		{0x09,   512},
+		{0x0A,  1024},
+		{0x0B,  2048},
+		{0x0C,  4096},
+		{0x0D,  8192},
+		{0x0E, 16384},
+		{0x0F, 32768},
+		{0x10, 65536}
+	};
+
+	// ID -> page_size
+	static constexpr std::map<size_t, size_t> PAGE_SIZE = {
+		{0x08,  16},
+		{0x09,  16},
+		{0x0A,  16},
+		{0x0B,  16},
+		{0x0C,  32},
+		{0x0D,  32},
+		{0x0E,  64},
+		{0x0F,  64},
+		{0x10, 128}
+	};
+
+	// ID -> addr_size
+	static constexpr std::map<size_t, size_t> ADDR_SIZE = {
+		{0x08, 1},
+		{0x09, 1},
+		{0x0A, 1},
+		{0x0B, 1},
+		{0x0C, 2},
+		{0x0D, 2},
+		{0x0E, 2},
+		{0x0F, 2},
+		{0x10, 2}
+	};
+
+	M24XXX_DRE_base(const M24XXX_DRE_Properties& prop, const std::shared_ptr<I2C_bus_base>& bus, const long id) : I2C_dev_base(bus, id)
 	{
 
 	}
