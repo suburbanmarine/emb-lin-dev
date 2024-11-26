8d9286ed4ac879aa5d8f6ca0013b23bda525a3be
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-11-26 04:04:15 -0500

    tcal9539

--- include/emb-lin-dev/TCAL9539.hpp
+++ include/emb-lin-dev/TCAL9539.hpp
@@ -18,6 +18,35 @@ class TCAL9539 : public I2C_dev_base
 public:
 	TCAL9539(const std::shared_ptr<I2C_bus_base>& bus, const long id);
 	~TCAL9539() override;
-	
+	enum class CMD_CODE : uint8_t
+	{
+		IN0          = 0x00U,
+		IN1          = 0x01U,
+		OUT0         = 0x02U,
+		OUT1         = 0x03U,
+		CONF0        = 0x06U,
+		CONF1        = 0x07U,
+		OUT_DRIVE0   = 0x40U,
+		OUT_DRIVE1   = 0x41U,
+		OUT_DRIVE2   = 0x42U,
+		OUT_DRIVE3   = 0x43U,
+		INPUT_LATCH0 = 0x44U,
+		INPUT_LATCH1 = 0x45U,
+		PU_PD_EN0    = 0x46U,
+		PU_PD_EN1    = 0x47U,
+		PU_PD_0      = 0x48U,
+		PU_PD_1      = 0x49U,
+		OUTPUT_CONF  = 0x4FU
+	};
+
+	bool read_input(uint16_t* const out_reg);
+	bool write_output(const uint16_t reg);
+
+	bool set_pin_input(const uint16_t reg);
+
+	bool set_pu_pd_en(const uint16_t reg);
+	bool set_pu_pd(const uint16_t reg);
+
 protected:
+	bool set_reg_16(const uint8_t a_low, const uint8_t a_high, const uint16_t reg);
 };
--- src/TCAL9539.cpp
+++ src/TCAL9539.cpp
@@ -11,11 +11,88 @@
 
 #include "emb-lin-dev/TCAL9539.hpp"
 
+#include <spdlog/spdlog.h>
+
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+extern "C"
+{
+	#include <i2c/smbus.h>
+}
+
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 TCAL9539::TCAL9539(const std::shared_ptr<I2C_bus_base>& bus, const long id) : I2C_dev_base(bus, id)
 {
 
 }
-TCAL9539::~TCAL9539() override
+TCAL9539::~TCAL9539()
 {
 
+}
+
+bool TCAL9539::read_input(uint16_t* const out_reg)
+{
+	std::shared_ptr<I2C_bus_open_close> bus_closer = std::make_shared<I2C_bus_open_close>(*m_bus);
+
+	if( ! m_bus->set_device_id(m_dev_addr) )
+	{
+		return false;
+	}
+
+	int32_t ret = i2c_smbus_read_byte_data(m_bus->get_fd(), uint8_t(CMD_CODE::IN0));
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	ret = i2c_smbus_read_byte_data(m_bus->get_fd(), uint8_t(CMD_CODE::IN1));
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+bool TCAL9539::write_output(const uint16_t reg)
+{
+	return set_reg_16(uint8_t(CMD_CODE::OUT0), uint8_t(CMD_CODE::OUT1), reg);
+}
+bool TCAL9539::set_pin_input(const uint16_t reg)
+{
+	return set_reg_16(uint8_t(CMD_CODE::CONF0), uint8_t(CMD_CODE::CONF1), reg);
+}
+bool set_pu_pd_en(const uint16_t reg)
+{
+	return set_reg_16(uint8_t(CMD_CODE::PU_PD_EN0), uint8_t(CMD_CODE::PU_PD_EN1), reg);
+}
+bool set_pu_pd(const uint16_t reg)
+{
+	return set_reg_16(uint8_t(CMD_CODE::PU_PD0), uint8_t(CMD_CODE::PU_PD1), reg);
+}
+
+bool TCAL9539::set_reg_16(const uint8_t a_low, const uint8_t a_high, const uint16_t reg)
+{
+	std::shared_ptr<I2C_bus_open_close> bus_closer = std::make_shared<I2C_bus_open_close>(*m_bus);
+
+	if( ! m_bus->set_device_id(m_dev_addr) )
+	{
+		return false;
+	}
+
+	int32_t ret = i2c_smbus_write_byte_data(m_bus->get_fd(), a_low, (reg & 0x00FFU) >> 0);
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	ret = i2c_smbus_write_byte_data(m_bus->get_fd(), a_high, (reg & 0xFF00U) >> 8);
+	if(ret < 0)
+	{
+		return false;
+	}
+
+	return true;	
 }
