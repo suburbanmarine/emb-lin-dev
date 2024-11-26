1f108021922b93d001f0987457178c5c6a8737d9
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-11-26 03:17:01 -0500

    tcal9539

---
+++ include/emb-lin-dev/TCAL9539.hpp
@@ -0,0 +1,23 @@
+/**
+ * This file is part of emb-lin-dev, mostly a collection of userspace drivers and helper utilities for embedded linux.
+ * 
+ * This software is distrubuted in the hope it will be useful, but without any warranty, including the implied warrranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE.txt for details.
+ * 
+ * @author Jacob Schloss <jacob.schloss@suburbanmarine.io>
+ * @copyright Copyright (c) 2024 Suburban Marine, Inc. All rights reserved.
+ * @license Licensed under the LGPL-3.0 license. See LICENSE.txt for details.
+ * SPDX-License-Identifier: LGPL-3.0-only
+*/
+
+#pragma once
+
+#include "emb-lin-dev/I2C_dev_base.hpp"
+
+class TCAL9539 : public I2C_dev_base
+{
+public:
+	TCAL9539(const std::shared_ptr<I2C_bus_base>& bus, const long id);
+	~TCAL9539() override;
+	
+protected:
+};
